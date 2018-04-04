6dd1247c4883d56526263b8e14d211648f905048
Tatiana K. Joergensen <hipporosa@gmail.com>
2018-04-04 10:36:04 +0100

    Add files via upload

---
+++ Global.asax.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace ReactMVC
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            //AreaRegistration.RegisterAllAreas();
+            //FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            ReactConfig.Configure();
+
+        }
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+            HttpContext.Current.Response.AddHeader("Access-Control-Allow-Origin", "*");
+        }
+    }
+}
